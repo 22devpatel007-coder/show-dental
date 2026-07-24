652ece19d779b9c94d40ffea8a1bbc24cb7ffbfc
Dev <22devpatel007@gmail.com>
2026-07-24 16:02:46 -0230

    Update

--- src/data/clinicTemplates.ts
+++ src/data/clinicTemplates.ts
@@ -192,7 +192,7 @@ export const clinicConfig: ClinicConfig = {
         saturday: 'Saturday: 9:00 AM - 2:00 PM',
         sunday: 'Sunday: Closed (On-call emergencies only)'
       },
-      whatsappNumber: '+919328691416',
+      whatsappNumber: '+919999999999',
       mapEmbedUrlUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.0298758804!2d-73.9822452!3d40.7613621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQ1JzQxLjAiTiA3M8KwNTgnNTYuMCJX!5e0!3m2!1sen!2sus!4v1234567890'
     }
   }
